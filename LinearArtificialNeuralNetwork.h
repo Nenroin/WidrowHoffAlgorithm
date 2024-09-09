4e07564d5616b5f2bf9f222de81bb1215aad557f
Andrey  Marzan <96544191+Nenroin@users.noreply.github.com>
2024-09-09 10:36:27 +0300

    Create base functional

--- LinearArtificialNeuralNetwork.h
+++ LinearArtificialNeuralNetwork.h
@@ -1,7 +1,32 @@
 #pragma once
+#include <vector>
+#include <iostream>
 
-class LinearArtificialNeuralNetwork
+using neural_layer_t = std::vector<double>;
+using neuron_t = double;
+
+class linear_artificial_neural_network
 {
+    std::vector<neural_layer_t> neural_network_;
 public:
+    linear_artificial_neural_network& add_neural_layer(const int neurons_number)
+    {
+        neural_network_.push_back(neural_layer_t(neurons_number));  // NOLINT(modernize-use-emplace)
+
+        return *this; 
+    }
     
+    void show_neural_network_structure() const
+    {
+        int neurons_number{ 0 }, idx{ 1 };
+        const int layer_number{ static_cast<int>(neural_network_.size()) } ;
+        
+        for(const auto &neural_layer : neural_network_)
+        {
+            neurons_number = static_cast<int>(neural_layer.size());
+            std::cout << neurons_number << (idx < layer_number ? " <-> " : "");
+            
+            ++idx;
+        }
+    }
 };
--- WidrowHoffAlgorithm.cpp
+++ WidrowHoffAlgorithm.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <conio.h>
+#include "LinearArtificialNeuralNetwork.h"
 
 int main()
 {
-    
+    linear_artificial_neural_network neural_network;
+    neural_network.add_neural_layer(2).add_neural_layer(5).add_neural_layer(4);
+    neural_network.show_neural_network_structure();
     
     std::cout << "\nPut any button to exit...";
     _getch();
