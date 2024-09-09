657df79d3edd66a3745ae0ed19837f3ac0994c9c
Andrey  Marzan <96544191+Nenroin@users.noreply.github.com>
2024-09-09 09:41:33 +0300

    Init

---
+++ LinearArtificialNeuralNetwork.cpp
@@ -0,0 +1,1 @@
+#include "LinearArtificialNeuralNetwork.h"
---
+++ WidrowHoffAlgorithm.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+    
+    
+    std::cout << "\nPut any button to exit...";
+    _getch();
+    
+    return 0;
+}
